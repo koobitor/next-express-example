f630694e768cce5397e8597fccf8b847b4e63b35
Sakol Assawasagool <koobitor@gmail.com>
2018-07-22 12:28:09 +0700

    demo rechart

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,29 @@ class Index extends React.Component {
     this.state = {
       bitcoin: this.props.bitcoin
     }
+
+    this.fetch = this.fetch.bind(this)
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.fetch()
+    }, 1000)
+  }
+
+  async fetch() {
+    const month = "0" + (Math.floor(Math.random() * 10) + 1)
+    const res = await fetch('https://api.coindesk.com/v1/bpi/historical/close.json?start=2018-'+month+'-01&end=2018-'+month+'-30')
+    const json = await res.json()
+    const data = Object.keys(json.bpi).map((item) => {
+      return {
+        key: item,
+        value: json.bpi[item]
+      }
+    })
+    this.setState({
+      bitcoin: data
+    })
   }
 
   render() {
